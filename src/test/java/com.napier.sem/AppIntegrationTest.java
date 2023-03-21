65e30af6bda3e9634617327539bd23e753b32df5
40544502 <40544502@live.napier.ac.uk>
2023-03-21 14:34:03 +0000

    AOB - integration tests update

--- src/test/java/com.napier.sem/AppIntegrationTest.java
+++ src/test/java/com.napier.sem/AppIntegrationTest.java
@@ -1,33 +1,59 @@
-//import com.napier.sem.App;
-//import com.napier.sem.Population;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-///**
-// * Unit Test to test worldPopulation is not null
-// */
-//public class IntegrationTests {
-//
-//    static App app;
-//    @BeforeAll
-//    static void init()
-//    {
-//        app = new App();
-//        // Connect to database
-//        app.connect("localhost:33060", 30000);
-//
-//    }
-//
-//    /**
-//     * Test connection to database and to test getting world population
-//     * Used in additional_info 1 report
-//     */
-//    @Test
-//    public void GetWorldPopulation() {
-//        Population pop = app.getPopulation();
-//        //validates
-//        assertNotEquals(0, pop.population );
-//    }
-//
-//}
+package com.napier.sem;
+
+import com.napier.sem.App;
+import com.napier.sem.Country;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Integration Test to test reports
+ */
+public class AppIntegrationTest {
+
+    static App app;
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        // Connect to database
+        app.connect("localhost:33060", 30000);
+
+    }
+
+    /**
+     * Test connection to database and to test getting world population
+     * Used in additional_info 1 report
+     */
+
+    /** Integration test for extract all world countries
+     * Author - AOB
+     */
+    @Test
+    public void testGetAllCountries(){
+        ArrayList<Country> country = app.getAllCountries();
+        assertNotNull(country);
+    }
+
+    /** integration test for extract specific continent countries
+     * Author - AOB
+     */
+    @Test
+    public void testGetContinentCountries(){
+        ArrayList<Country> country = app.getContinentCountries();
+        assertNotNull(country);
+    }
+
+    /** integration test for extract specific region countries
+     * Author - AOB
+     */
+    @Test
+    public void testGetRegionCountries() {
+        ArrayList<Country> country = app.getRegionCountries();
+        //validates
+
+        assertNotNull(country);
+    }
+}
