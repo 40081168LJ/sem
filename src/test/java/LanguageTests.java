42383aa88ef3e9400dbfd52d75733ae6894b1129
LJmorris <liam.morris@bsw.co.uk>
2023-03-21 08:03:06 +0000

    LJ - Updated testing. Basic test based.

--- src/test/java/LanguageTests.java
+++ src/test/java/LanguageTests.java
@@ -1,13 +1,35 @@
+import com.napier.sem.App;
+import com.napier.sem.Language;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import java.util.ArrayList;
 
-class LanguageTests {
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LanguageTests
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
 
     /**UNIT TEST FOR POPULATION OF "China/Chinese"
      *Finally, the organisation has asked if it is possible to provide the number of people who speak the following:
-     *The following languages from greatest number to smallest, including the percentage of the world population:
+     *The following languages from the largest number to smallest, including the percentage of the world population:
      */
 
-    @Test1
+    @Test
+    void displayCountryLanguage()
+    {
+        Language countryLanguageTest1 = new Language();
+        app.displayCountryLanguage(countryLanguageTest1);
+    }
 
 
 }
