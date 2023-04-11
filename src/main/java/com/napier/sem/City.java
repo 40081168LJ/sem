146685326df51b24bec07dede0159634290262d3
40544502 <40544502@live.napier.ac.uk>
2023-04-11 16:13:49 +0100

    AOB - report 16 method added to city class

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -467,6 +467,51 @@ public class City {
                 }
         }
 
+
+//--------------------------------------------------------------------------------------------------------------------//
+        /**
+         * gets the top populated cities with the number of rows selected and district given by user. - report 16
+         * @param selected the number of rows to be selected inputted by user
+         * @param district the country to select rows from
+         * @param con connection to database
+         * @return returns null if fail or a list of cities
+         */
+
+        public static ArrayList<City> getTopCitiesByDistrict(int selected, String district, Connection con) {
+                try {
+                        Statement stmt = con.createStatement();
+                        // Create string for SQL statement
+                        String strSelect =
+                                "SELECT city.Name, country.Name, city.District, city.Population " +
+                                        "FROM city " +
+                                        "JOIN country " +
+                                        "ON city.countrycode = country.Code " +
+                                        "WHERE city.district LIKE \"%" + district + "%\" " +
+                                        "ORDER BY city.population DESC " +
+                                        "LIMIT " + selected;
+                        // Execute SQL statement
+                        ResultSet rset = stmt.executeQuery(strSelect);
+
+                        // Extract city information
+                        ArrayList<City> cities = new ArrayList<>();
+
+                        //adds each city to cities
+                        while (rset.next()) {
+                                City city = new City();
+                                city.name = rset.getString("city.Name");
+                                city.country = rset.getString("country.Name");
+                                city.district = rset.getString("city.District");
+                                city.population = rset.getInt("city.Population");
+                                cities.add(city);
+                        }
+                        return cities;
+                } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        System.out.println("Failed to get city details");
+                        return null;
+                }
+        }
+
 //--------------------------------------------------------------------------------------------------------------------//
 }
 
