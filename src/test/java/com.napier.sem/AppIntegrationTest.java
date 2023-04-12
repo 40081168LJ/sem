7ab63e640b0d56e56ac4b3e3e3817ed0225f8b7c
Liam Morris <104654300+MorrieMan@users.noreply.github.com>
2023-04-12 23:13:51 +0100

    LJ - CodeCov Test - Report 20 + AI3 + AI5 + 15 + n/a (From line 312 Up to line 379) Checking n/a - Checking A15 - x Checking AI3 - x Checking 20 - ISSUE - Fix Report 20 Test Checking 15 -

--- src/test/java/com.napier.sem/AppIntegrationTest.java
+++ src/test/java/com.napier.sem/AppIntegrationTest.java
@@ -315,43 +315,43 @@ public class AppIntegrationTest {
     }
 
 //--------------------------------------------------------------------------------------------------------------------//
-//    /** integration test for getting cities when given by country when given number
-//     * of rows to select and a country
-//     * report 15
+    /** integration test for getting cities when given by country when given number
+     * of rows to select and a country
+     * report 15
+     */
+    @Test
+    public void getTopCitiesByCountry() {
+        ArrayList<City> cities = City.getTopCitiesByCountry(5, "France",app.con);
+        //validates
+        assertNotNull(cities);
+        assertNotNull(cities.get(0).name);
+        assertNotNull(cities.get(0).country);
+        assertNotNull(cities.get(0).district);
+    }
+
+//--------------------------------------------------------------------------------------------------------------------//
+//    /**
+//     * integration test for getting all district populations - Report Additional info 5
 //     */
 //    @Test
-//    public void getTopCitiesByCountry() {
-//        ArrayList<City> cities = City.getTopCitiesByCountry(5, "France",app.con);
-//        //validates
-//        assertNotNull(cities);
-//        assertNotNull(cities.get(0).name);
-//        assertNotNull(cities.get(0).country);
-//        assertNotNull(cities.get(0).district);
+//    public void GetDistrictPopulations() {
+//        ArrayList<Population> populations = Population.getcityPopulation(app.con);
+//        assertNotNull(populations);
+//        assertNotNull(populations.get(0).district);
+//
 //    }
 
 //--------------------------------------------------------------------------------------------------------------------//
-    /**
-     * integration test for getting all district populations - Report Additional info 5
-     */
-    @Test
-    public void GetDistrictPopulations() {
-        ArrayList<Population> populations = Population.getcityPopulation(app.con);
-        assertNotNull(populations);
-        assertNotNull(populations.get(0).district);
-
-    }
-
-//--------------------------------------------------------------------------------------------------------------------//
-    /**
-     * integration test for getting all Region populations - Report Additional info 3
-     */
-    @Test
-    public void getRegionPopulations() {
-        ArrayList<Population> populations = Population.getRegionPopulation(app.con);
-        assertNotNull(populations);
-        assertNotNull(populations.get(0).region);
-
-    }
+//    /**
+//     * integration test for getting all Region populations - Report Additional info 3
+//     */
+//    @Test
+//    public void getRegionPopulations() {
+//        ArrayList<Population> populations = Population.getRegionPopulation(app.con);
+//        assertNotNull(populations);
+//        assertNotNull(populations.get(0).region);
+//
+//    }
 
 //--------------------------------------------------------------------------------------------------------------------//
 //    /**
